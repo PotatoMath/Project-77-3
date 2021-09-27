6755105d59c637323eb4bf3fe5c75ecf1f0afc2a
PotatoMath <82294627+PotatoMath@users.noreply.github.com>
2021-09-28 09:16:59 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+        var first_name = "Riya";
+        var last_name = "Bavi";
+        var full_name = first_name.concat(last_name);
+        console.log(full_name);
+
+        var a = 2;
+        var b = 6;
+        var add = a + b ;
+        console.log(add);
